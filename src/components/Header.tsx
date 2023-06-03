5584792a6d4cbdcd3c6a8f0a0e33953ed434a9a7
Anugrah Mishra <anugrah.mishra@globallogic.com>
2023-06-03 13:41:49 +0530

    removed unused import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import { signOut } from "../actions/AuthActions";
 
